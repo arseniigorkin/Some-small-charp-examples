67fd0cd466eb1b496b4a36a23dad2ab2841f64dd
Arsenii Gorkin <gorkin@protonmail.com>
2023-02-12 08:49:01 +0200

    Added a few headers and some query pickups

--- WebApplicationFromEmpty/Program.cs
+++ WebApplicationFromEmpty/Program.cs
@@ -1,6 +1,34 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+
 var builder = WebApplication.CreateBuilder(args);
+// builder.WebHost.ConfigureKestrel(option =>
+// {
+// 	option.ConfigureEndpointDefaults(lo => lo.Protocols = HttpProtocols.Http2);
+//
+// });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World 2!");
 
+// app.MapGet("/", () => "Hello World 2!");
+app.Run(async (HttpContext context) =>
+	{	
+		context.Response.Headers["Content-type"] = "text/html";
+		// context.Response.StatusCode = 400;
+		// context.Response.Headers["Name"] = "SomeName";
+		await context.Response.WriteAsync("<h2>Hello!</h2>");
+		if (context.Request.Query.ContainsKey("somekey"))
+		{
+			string someval = context.Request.Query["somekey"];
+			await context.Response.WriteAsync($"seomekey is set to <b>{someval}</b>.<br />");
+		}
+
+		if (context.Request.Headers.ContainsKey("Name"))
+		{
+			string hVal = context.Request.Headers["Name"];
+			await context.Response.WriteAsync($"The Name in the headers is set to: {hVal}.<br />");
+		}
+	}
+);
 app.Run();
